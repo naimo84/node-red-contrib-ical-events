4c0ac75c4a58f3d73bcbba016fddcc85fc0e6c5a
naimo84 <git@neumann-benjamin.de>
2019-08-09 09:55:46 +0200

    improved cron handling

--- dist/ical-events.js
+++ dist/ical-events.js
@@ -1,13 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var crypto = require("crypto-js");
 var ical = require('node-ical');
 var CronJob = require('cron').CronJob;
 module.exports = function (RED) {
     var configNode;
+    var newCronJobs = new Map();
+    var startedCronJobs = new Map();
     function eventsNode(config) {
         RED.nodes.createNode(this, config);
         configNode = RED.nodes.getNode(config.confignode);
-        var job = new CronJob(config.cron || '0 * * * * *', cronCheckJob.bind(null, this, config));
+        var job = new CronJob(config.cron || '0,30 * * * * *', cronCheckJob.bind(null, this, config));
         job.start();
     }
     function cronCheckJob(node, config) {
@@ -22,7 +25,7 @@ module.exports = function (RED) {
                     var ev = data[k];
                     var eventStart = new Date(ev.start);
                     if (ev.type == 'VEVENT') {
-                        if (eventStart.getTime() >= dateNow.getTime()) {
+                        if (eventStart.getTime() > dateNow.getTime()) {
                             var event_1 = {
                                 summary: ev.summary,
                                 location: ev.location,
@@ -31,17 +34,31 @@ module.exports = function (RED) {
                             if (config.offset) {
                                 eventStart.setMinutes(eventStart.getMinutes() + parseInt(config.offset));
                             }
-                            try {
-                                var job2 = new CronJob(eventStart, cronJob.bind(null, event_1, node));
-                                job2.start();
+                            else {
+                                eventStart.setMinutes(eventStart.getMinutes() - 1);
                             }
-                            catch (job_err) {
-                                node.error(job_err);
+                            var job2 = new CronJob(eventStart, cronJob.bind(null, event_1, node));
+                            var uid = crypto.MD5(ev.start + ev.summary).toString();
+                            if (ev.uid) {
+                                uid = ev.uid;
+                            }
+                            if (!newCronJobs.has(uid) && !startedCronJobs.has(uid)) {
+                                newCronJobs.set(uid, job2);
+                                console.debug("new - " + uid);
+                            }
+                            else if (startedCronJobs.has(uid)) {
+                                console.debug("started - " + uid);
                             }
                         }
                     }
                 }
             }
+            console.debug(newCronJobs.size);
+            newCronJobs.forEach(function (job, key) {
+                job.start();
+                startedCronJobs.set(key, job);
+            });
+            newCronJobs.clear();
         });
     }
     function cronJob(event, node) {
--- src/ical-events.ts
+++ src/ical-events.ts
@@ -1,5 +1,6 @@
 
 import { Red, Node } from 'node-red';
+import * as crypto from "crypto-js";
 var ical = require('node-ical');
 const CronJob = require('cron').CronJob;
 
@@ -7,31 +8,43 @@ export interface Config {
     url: string,
 }
 
+export interface Job {
+    id: string,
+    cronjob: any
+}
+
+
 module.exports = function (RED: Red) {
     let configNode: Config;
+
+    let newCronJobs = new Map();
+    let startedCronJobs = new Map();
+
 
     function eventsNode(config: any) {
         RED.nodes.createNode(this, config);
         configNode = RED.nodes.getNode(config.confignode) as unknown as Config;
-        const job = new CronJob(config.cron || '0 * * * * *', cronCheckJob.bind(null, this, config));
+        const job = new CronJob(config.cron || '0,30 * * * * *', cronCheckJob.bind(null, this, config));
         job.start();
-
     }
+
 
     function cronCheckJob(node: Node, config: any) {
         var dateNow = new Date();
 
+
         ical.fromURL(configNode.url, {}, function (err, data) {
-            if(err){
+            if (err) {
                 node.error(err);
                 return;
             }
             for (let k in data) {
                 if (data.hasOwnProperty(k)) {
                     var ev = data[k];
+
                     const eventStart = new Date(ev.start);
                     if (ev.type == 'VEVENT') {
-                        if (eventStart.getTime() >= dateNow.getTime()) {
+                        if (eventStart.getTime() > dateNow.getTime()) {
                             const event = {
                                 summary: ev.summary,
                                 location: ev.location,
@@ -40,22 +53,39 @@ module.exports = function (RED: Red) {
 
                             if (config.offset) {
                                 eventStart.setMinutes(eventStart.getMinutes() + parseInt(config.offset));
+                            } else {
+                                eventStart.setMinutes(eventStart.getMinutes() - 1);
                             }
 
-                            try {
-                                const job2 = new CronJob(eventStart, cronJob.bind(null, event, node));
-                                job2.start();
-                            } catch (job_err) {
-                                node.error(job_err);
+                            const job2 = new CronJob(eventStart, cronJob.bind(null, event, node));
+
+                            let uid = crypto.MD5(ev.start + ev.summary).toString();
+                            if (ev.uid) {
+                                uid = ev.uid;
                             }
+
+                            if (!newCronJobs.has(uid) && !startedCronJobs.has(uid)) {
+                                newCronJobs.set(uid, job2);
+                                console.debug("new - " + uid);
+                            }
+                            else if (startedCronJobs.has(uid)) {
+                                console.debug("started - " + uid);
+                            }
+
                         }
                     }
                 }
             }
+
+            console.debug(newCronJobs.size);
+            newCronJobs.forEach((job, key) => {
+                job.start();
+                startedCronJobs.set(key, job);
+            });
+            newCronJobs.clear();
         });
-
-
     }
+
 
     function cronJob(event: any, node: Node) {
         node.send({
